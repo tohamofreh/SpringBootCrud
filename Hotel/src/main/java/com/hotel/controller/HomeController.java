79b62e376a5228e9d5e3a7caab9d1f3a940e395c
tohamofreh <fathyamofreh@gmail.com>
2018-08-06 00:47:22 +0200

    first commit

---
+++ Hotel/src/main/java/com/hotel/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.hotel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class HomeController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String gethome(){
+
+        return "home";
+    }
+
+}
